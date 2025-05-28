2a56f4463c1d42b99e1e4f6ec94e4c4160e2d890
aiswarya <ashzzaishwarya@gmail.com>
2025-05-29 01:21:02 +0530

    b8 button added

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <button>b7</button>
+      <button>b8</button>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
